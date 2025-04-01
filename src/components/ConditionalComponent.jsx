47795d8caceda334424ed38c4eacad2e3c7d1c92
Hilarion13225 <yaokonan475@gmail.com>
2025-04-01 01:31:49 +0000

    ReactJs Tuto17

---
+++ src/components/ConditionalComponent.jsx
@@ -0,0 +1,13 @@
+import Code from "./Code";
+import Welcome from "./Welcome";
+
+export default function ConditionalComponent() {
+  let message;
+  const display = true;
+  if (display) {
+    message = <h1>This is message 1</h1>;
+  } else {
+    message = <h1>This is message 2</h1>;
+  }
+  return message;
+}
